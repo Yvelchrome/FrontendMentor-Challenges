0e9d83736e76fe0fbd8d7d21e66b96caa18b8d04
Yvelchrome <yvelchrome@gmail.com>
2022-07-22 15:52:44 +0200

    feat: main js script

---
+++ script/main.js
@@ -0,0 +1,12 @@
+let buttons = document.getElementsByClassName(
+  "card__rating__container--button"
+);
+
+document.addEventListener("click", function (evt) {
+  if (evt.target.classList.contains("card__rating__container--button")) {
+    Array.prototype.forEach.call(buttons, function (button) {
+      button.classList.remove("active");
+    });
+    evt.target.classList.add("active");
+  }
+});
