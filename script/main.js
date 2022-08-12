9ab6061be87d9b650c93289ae0a2b524511be83c
Yvelchrome <yvelchrome@gmail.com>
2022-08-12 12:23:02 +0200

    feat: toggle the display of the submenus on click

--- script/main.js
+++ script/main.js
@@ -1,13 +1,59 @@
 function triggerHamburger() {
-  let hamburger = document.querySelector(".hamburger");
-  let mobileNav = document.querySelector(".mobile-nav");
-  let mobileBackground = document.querySelector(".mobile-background");
+  const hamburger = document.querySelector(".hamburger");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const mobileBackground = document.querySelector(".mobile-background");
 
-  window.onload = hamburger.addEventListener("click", function () {
+  hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("active");
     mobileNav.classList.toggle("mobile-nav--active");
     mobileBackground.classList.toggle("mobile-background--active");
   });
 }
 
-window.onload = triggerHamburger;
+function triggerFeatures() {
+  const featuresButton = document.querySelector(".navbar__dropdown--features");
+  const features = document.querySelector(
+    ".navbar__dropdown__submenu--features"
+  );
+  const dropdownArrow = document.querySelector(".dropdownArrow--features");
+
+  featuresButton.addEventListener("click", function () {
+    features.classList.toggle("active");
+    dropdownArrow.classList.toggle("active");
+  });
+  document.addEventListener("click", function (e) {
+    if (
+      !e.target.closest(".navbar__dropdown--features") &&
+      !e.target.closest(".navbar__dropdown__submenu--features") &&
+      !e.target.closest(".dropdownArrow--features")
+    ) {
+      features.classList.remove("active");
+      dropdownArrow.classList.remove("active");
+    }
+  });
+}
+
+function triggerCompany() {
+  const companyButton = document.querySelector(".navbar__dropdown--company");
+  const company = document.querySelector(".navbar__dropdown__submenu--company");
+  const dropdownArrow = document.querySelector(".dropdownArrow--company");
+
+  companyButton.addEventListener("click", function () {
+    company.classList.toggle("active");
+    dropdownArrow.classList.toggle("active");
+  });
+  document.addEventListener("click", function (e) {
+    if (
+      !company.contains(e.target) &&
+      !companyButton.contains(e.target) &&
+      company.classList.contains("active")
+    ) {
+      company.classList.remove("active");
+      dropdownArrow.classList.remove("active");
+    }
+  });
+}
+
+triggerHamburger();
+triggerFeatures();
+triggerCompany();
